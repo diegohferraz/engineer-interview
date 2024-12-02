7d1f205b5d0c48c00da9fbd125299e68dff7896f
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2024-12-02 18:48:51 -0300

    refactor: code clean up and change css to css modules

--- src/ChallengeComponent.tsx
+++ src/ChallengeComponent.tsx
@@ -1,10 +1,1 @@
-import React from 'react'
-
-export function ChallengeComponent() {
-  return (
-    <>
-      {/* Delete this h2, and add your own code here. */}
-      <h2 style={{ textAlign: 'center', paddingTop: 200, paddingBottom: 200 }}>Your code goes here</h2>
-    </>
-  )
-}
+export const ChallengeComponent = () => <h2>Hello Every.io</h2>;
